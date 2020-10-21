02045f15fdbb9c99d693d4456815723b2e7742cf
Randall Teves <rteves0914@gmail.com>
2020-10-21 00:00:50 -0700

    Finished polling code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -35,13 +35,38 @@ with open(csvpath) as csvfile:
             candidate_L +=1
         if row[2] == "O'Tooley":
             candidate_O +=1
-        # calculate the percentage of vote that each candidate received
 
-    print(f"There are {votes} total votes on this election data.")
-    print(candidates)
-    print(f"Correy received {candidate_C} votes.")
-    print(f"Khan received {candidate_K} votes.")
-    print(f"Li received {candidate_L} votes.")
-    print(f"O'Tooley received {candidate_O} votes.")
+    # calculate the percentage of vote that each candidate received
+    dec_C = (candidate_C / votes)
+    pct_C = "{:3%}".format(dec_C)
 
-        # print the winner of the election
+    dec_K = candidate_K / votes
+    pct_K = "{:3%}".format(dec_K)
+
+    dec_L = candidate_L / votes
+    pct_L = "{:3%}".format(dec_L)
+
+    dec_O = candidate_O / votes
+    pct_O = "{:3%}".format(dec_O)
+
+    # print out all the important information
+    print("Here are the election results:")
+    print("-------------")
+    print(f"There were a total of {votes} votes casted in this election.")
+    print("-------------")
+    print(f"Correy received {candidate_C} total votes, or {pct_C}.")
+    print(f"Khan received {candidate_K} votes, or {pct_K}.")
+    print(f"Li received {candidate_L} votes, or {pct_L}.")
+    print(f"O'Tooley received {candidate_O} votes, or {pct_O}.")
+    print("-------------")
+
+    # print the winner of the election
+    if candidate_C > candidate_K and candidate_L and candidate_O:
+        winner = 'Correy'
+    if candidate_K > candidate_C and candidate_L and candidate_O:
+        winner = 'Khan'
+    if candidate_L > candidate_C and candidate_K and candidate_O:
+        winner = 'Li'
+    if candidate_O > candidate_C and candidate_K and candidate_L:
+        winner = "O'Tooley"
+    print(f"And the winner of the election is {winner}! Congratulations {winner}, and good job to the other candidates!")
