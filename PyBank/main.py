c317e13fe4f7e038f9106df679a3dc6d7169b08f
Randall Teves <rteves0914@gmail.com>
2020-10-21 22:53:32 -0700

    Finished writing code, need text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,6 +14,7 @@ with open(csvpath) as csvfile:
     total_profits = 0
     profit_list = []
     profit_changes = []
+    date_list = []
     i = 0
 
     # print the csv file and the number of months on report
@@ -27,6 +28,9 @@ with open(csvpath) as csvfile:
         total_profits += int(row[1])
         profit_list.append(int(row[1]))
 
+        # store all the dates on a list
+        date_list.append(str(row[0]))
+
     # create the list that keeps track of all the monthly changes
     for i in range((months)-1):
         profit_changes.append((profit_list[i+1])-(profit_list[i]))
@@ -37,13 +41,18 @@ with open(csvpath) as csvfile:
     # find the greatest increase and decrease over the entire time period
     increase_change = max(profit_changes)
     decrease_change = min(profit_changes)
-    
 
+    # find the month and year that the greatest increase and decrease happened
+    increase_index = profit_changes.index(increase_change) + 1
+    decrease_index = profit_changes.index(decrease_change) + 1
+    increase_date = date_list[increase_index]
+    decrease_date = date_list[decrease_index]
+    
     print("Here is the financial analysis:")
     print("--------------")
     print(f"There are a total of {months} months on this budget data.")
     print("--------------")
     print(f"The net total of the company is ${total_profits}.")
     print(f"The average change per month is ${average_change}.")
-    print(f"The greatest increase in revenue is ${increase_change} and occurred in ___.")
-    print(f"The greatest decrease in revenue is ${decrease_change} and occurred in ___.")
+    print(f"The greatest increase in revenue is ${increase_change} and occurred in {increase_date}.")
+    print(f"The greatest decrease in revenue is ${decrease_change} and occurred in {decrease_date}.")
